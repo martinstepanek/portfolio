84e11b17c4b56447ba232a3ae19711711707d140
Martin Štěpánek <mstepanek015@gmail.com>
2019-02-17 20:37:49 +0100

    Fix production app building

--- webpack.config.js
+++ webpack.config.js
@@ -3,71 +3,77 @@ const HWP = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurifyCSSPlugin = require('purifycss-webpack');
 
-let plugins = [
-    new HWP({
-        template: './public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
-    }),
-];
 
-if (process.env.NODE_ENV === 'production') {
-    plugins.push(new PurifyCSSPlugin({
-        paths: [path.join(__dirname, 'dist/index.html')],
-    }));
-}
+module.exports = (env, argv) => {
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'build.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader
-                    }, {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true,
-                            includePaths: ['/scss']
-                        },
+    let plugins = [
+        new HWP({
+            template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+    ];
 
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
-            }
-        ]
-    },
-    plugins: plugins,
-    devServer: {
-        port: 3000,
-        historyApiFallback: {
-            index: './public/index.html'
-        }
+    if (argv.mode === 'production') {
+        plugins.push(new PurifyCSSPlugin({
+            paths: [path.join(__dirname, 'dist/index.html')],
+        }));
     }
+
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'build.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: argv.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader
+                        }, {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                                includePaths: ['/scss']
+                            },
+
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }]
+                },
+                
+            ]
+        },
+        plugins: plugins,
+        devServer: {
+            port: 3000,
+            historyApiFallback: {
+                index: './public/index.html'
+            }
+        }
+    };
 };
