8d95c42cf7a4d216877266211161b1bd61b6a0a3
Martin Štěpánek <mstepanek015@gmail.com>
2019-02-17 14:34:52 +0100

    Add file loader config

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,16 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
         ]
     },
     plugins: plugins,
