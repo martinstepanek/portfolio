d313e29d3cad9a8e06ceb63577bb6a05baf74ca7
Martin Štěpánek <mstepanek015@gmail.com>
2019-02-17 20:14:35 +0100

    Add scroll spy for navbar

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import('../scss/style.scss');
 import('../scss/bootstrap/bootstrap-reboot.scss');
 import('../scss/bootstrap/bootstrap.scss');
 import('../scss/bootstrap/bootstrap-grid.scss');
+
+import('bootstrap/dist/js/bootstrap.min');
+
