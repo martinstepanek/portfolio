68e57efcf8b95dd4ce9fe8c2b32861a3a915b14f
Martin Štěpánek <mstepanek015@gmail.com>
2020-03-27 15:00:12 +0100

    fix: move style import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import('../scss/style.scss');
 import('../scss/bootstrap/bootstrap-reboot.scss');
 import('../scss/bootstrap/bootstrap.scss');
 import('../scss/bootstrap/bootstrap-grid.scss');
+import('../scss/style.scss');
 
 import('bootstrap/dist/js/bootstrap.min');
 
